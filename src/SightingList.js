4e18a4edf03a6723f72cad0014942b0a4c5a609a
Mikael Janhonen <janhon3n@gmail.com>
2018-01-11 23:14:19 +0200

    Create SightingList.js
    - Fetches sightings from the API
    - Renders a list of SightingListItem components

---
+++ src/SightingList.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import urljoin from 'url-join'
+import { withStyles } from 'material-ui/styles';
+import { CircularProgress } from 'material-ui/Progress';
+import Paper from 'material-ui/Paper';
+
+import SightingListItem from './SightingListItem'
+import ErrorMessage from './ErrorMessage'
+
+const styles = theme => ({
+  SightingList: {
+    padding:'5px'
+  }
+})
+
+class SightingList extends Component {
+
+  constructor(props) {
+    super(props)
+    this.fetchIntervalId = null
+
+    this.state = {
+      sightings: null,
+      apiFetchError: null
+    }
+  }
+
+
+  fetchSightingsFromTheApi() {
+    //fetch sightings from the API and add them to the state
+    fetch(urljoin(window.apiUrl, window.sightingsPath))
+      .then((response) => {
+        if (!response.ok) {
+          throw Error()
+        }
+        return response.json()
+      })
+      .then((responseInJson) => {
+        this.setState({ sightings: responseInJson, apiFetchError: null })
+      }).catch((error) => {
+        this.setState({ apiFetchError: new Error('Failed to fetch content from the api') })
+      })
+  }
+
+
+  componentDidMount() {
+    this.fetchIntervalId = window.setInterval(() => {
+      this.fetchSightingsFromTheApi()
+    }, 5000)
+    this.fetchSightingsFromTheApi()
+  }
+
+
+  render() {
+    if (this.state.apiFetchError !== null) {
+      return (
+        <Paper elevation={this.props.elevation} className={this.props.classes.SightingList + ' ' + this.props.className}>
+          <ErrorMessage message={this.state.apiFetchError.message} />
+        </Paper>
+      )
+    } else if (this.state.sightings === null) {
+      return (
+          <CircularProgress/>
+      )
+    }
+
+    return (
+      <div className={this.props.classes.SightingList + ' ' + this.props.className}>
+        {
+          this.state.sightings.map((sighting) => {
+            return (<SightingListItem sighting={sighting} />)
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(SightingList);
